10acf8accec81cb025910a53750e8302ca0e433e
hoannv4 <hoannv@edmicro.vn>
2021-05-25 15:06:34 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './componets/TaskList'
+import TaskList from './componets/TaskList';
+import FormTask from './componets/FormTask'
+import FindTask from './componets/FindTask'
+import Sort from './componets/Sort'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+  <div className="text-center">
+    <h1>Quản Lý Công Việc</h1>
+    <hr />
+  </div>
+  <div className="row">
+    <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <FormTask>
+
+        </FormTask>
     </div>
+    <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+      <button type="button" className="btn btn-primary">
+        <span className="fa fa-plus mr-5" />Thêm Công Việc
+      </button>
+      <div className="row mt-15">
+        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <FindTask></FindTask>
+        </div>
+        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <Sort>
+
+          </Sort>
+        </div>
+      </div>
+      <div className="row mt-15">
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <TaskList>
+            
+          </TaskList>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
   );
 }
 
